5865787b3a918fe958c54dcf192da4c9150fc5f7
Irgey <116591589+Irgey@users.noreply.github.com>
2023-04-02 21:26:33 +0200

    important fix for localStorage, async/await syntax

--- src/js/get-cocktail.js
+++ src/js/get-cocktail.js
@@ -1,11 +1,20 @@
 const BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
-export function getCocktail(cocktailName) {
-  return fetch(`${BASE_URL}${cocktailName}`).then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error();
-    }
-  });
+export async function getCocktail(cocktailName) {
+  const response = await fetch(`${BASE_URL}${cocktailName}`);
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error();
+  }
 }
+
+// export function getCocktail(cocktailName) {
+//   return fetch(`${BASE_URL}${cocktailName}`).then(response => {
+//     if (response.ok) {
+//       return response.json();
+//     } else {
+//       throw new Error();
+//     }
+//   });
+// }
