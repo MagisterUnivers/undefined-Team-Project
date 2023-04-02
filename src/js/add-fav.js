0503a9b59f9d095d999b53c28fa65b3555c4967d
Irgey <116591589+Irgey@users.noreply.github.com>
2023-04-02 18:32:18 +0200

    notifications and catching errors added

--- src/js/add-fav.js
+++ src/js/add-fav.js
@@ -1,3 +1,5 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
 import { getCocktail } from './get-cocktail';
 const refs = {
   cocktailsDiv: document.querySelector('#rnd-cocktail'),
@@ -15,11 +17,21 @@ function onBtnAddFavClick(e) {
       e.target.parentNode.parentNode.children.cocktailName.textContent;
     console.log(favCocktail);
     console.log(getCocktail(favCocktail));
-    getCocktail(favCocktail).then(result => {
-      console.log(result);
-      const favList = JSON.parse(localStorage.getItem('favCocktails'));
-      favList.push(result.drinks[0]);
-      localStorage.setItem('favCocktails', JSON.stringify(favList));
-    });
+    getCocktail(favCocktail)
+      .then(result => {
+        console.log(result);
+        const favList = JSON.parse(localStorage.getItem('favCocktails'));
+        favList.push(result.drinks[0]);
+        localStorage.setItem('favCocktails', JSON.stringify(favList));
+        return favList[favList.length - 1].strDrink;
+      })
+      .then(result => {
+        console.log(result);
+        Notify.success(`Cocktail ${result} added to your favorites!✅`);
+      })
+      .catch(error => {
+        console.log('ERROR>>', error.message);
+        Notify.failure('Some error has occurred. Try in a few minutes.❌');
+      });
   }
 }
