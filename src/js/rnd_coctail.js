e187eaca11cfb46b00fcd2dfa6e7d5eb36ed825c
Irgey <116591589+Irgey@users.noreply.github.com>
2023-04-03 10:21:32 +0200

    checking localStorage on including cocktail, constant.js created

--- src/js/rnd_coctail.js
+++ src/js/rnd_coctail.js
@@ -5,7 +5,7 @@
 */
 
 import '../sass/utils/_ui-kit.scss';
-
+import { ADD_BTN, REMOVE_BTN, LEARN_MORE_BTN } from './constants';
 /**
   |============================
   | Refs
@@ -141,25 +141,15 @@ export async function rndCocktails() {
 // }
 
 function createPic(picture) {
-  return `<li class="random-cocktail__item">
-  <img class="random-cocktail__image" src="${picture.drinks[0].strDrinkThumb}" alt="${picture.drinks[0].strCategory}" loading="lazy" width=0 heigth=0/><h3 class="random-cocktail__uppertext" name="cocktailName">${picture.drinks[0].strDrink}</h3><div class="random-cocktail__btn"><button type="button" class="btn btn-secondary">Learn more</button><button type="button" class="btn-primary btn btn-add-to">
-  Add to
-  <svg
-    class="btn-primary__icon btn-add-to"
-    viewBox="0 0 19 17"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      class="outer"
-      d="M9.5 17L8.1225 15.7771C3.23 11.4507 0 8.59727 0 5.09537C0 2.24196 2.299 0 5.225 0C6.878 0 8.4645 0.750409 9.5 1.93624C10.5355 0.750409 12.122 0 13.775 0C16.701 0 19 2.24196 19 5.09537C19 8.59727 15.77 11.4507 10.8775 15.7864L9.5 17Z"
-      fill="#FD5103"
-    />
-    <path
-      class="inner"
-      d="M9.50005 15.2106L8.38493 14.2452C4.42433 10.8296 1.80957 8.57687 1.80957 5.81221C1.80957 3.55952 3.67067 1.78955 6.03933 1.78955C7.37748 1.78955 8.66178 2.38198 9.50005 3.31816C10.3383 2.38198 11.6226 1.78955 12.9608 1.78955C15.3294 1.78955 17.1905 3.55952 17.1905 5.81221C17.1905 8.57687 14.5758 10.8296 10.6152 14.2525L9.50005 15.2106Z"
-      fill="#FD5103"
-    />
-  </svg>
-</button></div></li></div></div>`;
+  if (
+    localStorage
+      .getItem('favCocktails')
+      .includes(`${picture.drinks[0].idDrink}`)
+  ) {
+    return `<li class="random-cocktail__item">
+  <img class="random-cocktail__image" src="${picture.drinks[0].strDrinkThumb}" alt="${picture.drinks[0].strCategory}" loading="lazy" width=0 heigth=0/><h3 class="random-cocktail__uppertext" name="cocktailName">${picture.drinks[0].strDrink}</h3><div class="random-cocktail__btn">${LEARN_MORE_BTN}${REMOVE_BTN}</div></li></div></div>`;
+  } else {
+    return `<li class="random-cocktail__item">
+  <img class="random-cocktail__image" src="${picture.drinks[0].strDrinkThumb}" alt="${picture.drinks[0].strCategory}" loading="lazy" width=0 heigth=0/><h3 class="random-cocktail__uppertext" name="cocktailName">${picture.drinks[0].strDrink}</h3><div class="random-cocktail__btn">${LEARN_MORE_BTN}${ADD_BTN}</div></li></div></div>`;
+  }
 }
