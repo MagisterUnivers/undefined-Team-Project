66ce162e7d1a8295684ea19d883367af40b2adee
Irgey <116591589+Irgey@users.noreply.github.com>
2023-04-04 17:07:54 +0200

    logic created

--- src/js/modal-ingredients.js
+++ src/js/modal-ingredients.js
@@ -1,31 +1,85 @@
+/*
+  |============================
+  | Imports
+  |============================
+*/
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+/*
+  |============================
+  | Variables
+  |============================
+*/
 const modal = document.querySelector('#modal-ingredients');
 const openBtn = document.querySelector('.open-btn');
 const closeBtn = document.querySelector('.modal-ingredients-close');
 const modalGroup = document.querySelector('.backdrop');
 const removeFavoritesButton = document.querySelector('#modal-btn__remove-fav');
 const addFavoritesButton = document.querySelector('#modal-btn__add-to-fav');
+let ingredientTitle;
+let ingredient;
+if (!localStorage.getItem('favIngredients')) {
+  localStorage.setItem('favIngredients', JSON.stringify([]));
+}
+
 removeFavoritesButton.style.display = 'none';
 addFavoritesButton.addEventListener('click', () => {
   removeFavoritesButton.style.display = 'block';
   addFavoritesButton.style.display = 'none';
+  /*
+    |============================
+    | Serhii adding to localStorage logic
+    |============================
+  */
+  const parsedFavIngredients = JSON.parse(
+    localStorage.getItem('favIngredients')
+  );
+  parsedFavIngredients.push(ingredient);
+  console.log(parsedFavIngredients);
+  localStorage.setItem('favIngredients', JSON.stringify(parsedFavIngredients));
+  Notify.success(`${ingredient.strIngredient} added to your favorites😍!`);
 });
-removeFavoritesButton.addEventListener('click', () => {
+removeFavoritesButton.addEventListener('click', e => {
   removeFavoritesButton.style.display = 'none';
   addFavoritesButton.style.display = 'block';
+  const parsedFavIngredients = JSON.parse(
+    localStorage.getItem('favIngredients')
+  );
+  console.log(parsedFavIngredients);
+  console.dir(e.target);
+  parsedFavIngredients.splice(
+    parsedFavIngredients.findIndex(
+      ingredient =>
+        Number(ingredient.idIngredient) === Number(ingredientTitle.id)
+    ),
+    1
+  );
+  localStorage.setItem('favIngredients', JSON.stringify(parsedFavIngredients));
+  Notify.info(`${ingredient.strIngredient} was removed from your favorites🙄!`);
 });
 async function getIngredientData(ingredientName) {
   const response = await fetch(
     `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${ingredientName}`
   );
   const data = await response.json();
-
-  const ingredient = data.ingredients[0];
+  /*
+    |============================
+    | var ingredient moved to global scope
+    |============================
+  */
+  // const ingredient = data.ingredients[0];
+  ingredient = data.ingredients[0];
+  /*
+    |============================
+    | 
+    |============================
+  */
   let abv = `<li class="modal-ingredient-item specs-list__item"><span class="specs-list__marker">&#10038;</span> Alcohol by volume: ${ingredient.strABV}%</li>`;
   if (ingredient.strAlcohol === 'No') {
     abv = '';
   }
   function updateIngredients() {
-    return `<h2 class="modal-ingredient-title">${ingredient.strIngredient}</h2>
+    return `<h2 class="modal-ingredient-title" id='${ingredient.idIngredient}''>${ingredient.strIngredient}</h2>
         <strong class="modal-ingredient-uptitle">${ingredient.strType}</strong>
         <p class="modal-ingredient-text">${ingredient.strDescription}</p>
         <ul class="modal-ingredient-list specs-list">
@@ -34,7 +88,8 @@ async function getIngredientData(ingredientName) {
           </ul>`;
   }
 
-  modal.insertAdjacentHTML('beforeend', updateIngredients());
+  modal.insertAdjacentHTML('afterbegin', updateIngredients());
+  ingredientTitle = document.querySelector('.modal-ingredient-title');
 }
 
 closeBtn.addEventListener('click', toggleModal);
