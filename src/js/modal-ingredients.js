10ebab69016773a9b83b333bf660f6e51015bd2e
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-04-04 23:14:27 +0300

    Cleaning patch

--- src/js/modal-ingredients.js
+++ src/js/modal-ingredients.js
@@ -58,6 +58,13 @@ removeFavoritesButton.addEventListener('click', e => {
   localStorage.setItem('favIngredients', JSON.stringify(parsedFavIngredients));
   Notify.info(`${ingredient.strIngredient} was removed from your favorites🙄!`);
 });
+
+/**
+  |============================
+  | Julia logic
+  |============================
+*/
+
 export async function getIngredientData(ingredientName) {
   const response = await fetch(
     `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${ingredientName}`
@@ -77,17 +84,17 @@ export async function getIngredientData(ingredientName) {
   */
 
   let type = '';
-if (ingredient.strType) {
-  type = `<strong class="modal-ingredient-uptitle theme_text_color">${ingredient.strType}</strong>`;
-}
-let description = '';
-if(ingredient.strDescription){
-  description = `<p class="modal-ingredient-text theme_modal_text_color">${ingredient.strDescription}</p>`
-}
-let abv = '';
-if (ingredient.strABV) {
-  abv = `<li class="modal-ingredient-item specs-list__item theme_modal_text_color"><span class="specs-list__marker theme_text_color">&#10038;</span> Alcohol by volume: ${ingredient.strABV}%</li>`;
-}
+  if (ingredient.strType) {
+    type = `<strong class="modal-ingredient-uptitle theme_text_color">${ingredient.strType}</strong>`;
+  }
+  let description = '';
+  if (ingredient.strDescription) {
+    description = `<p class="modal-ingredient-text theme_modal_text_color">${ingredient.strDescription}</p>`;
+  }
+  let abv = '';
+  if (ingredient.strABV) {
+    abv = `<li class="modal-ingredient-item specs-list__item theme_modal_text_color"><span class="specs-list__marker theme_text_color">&#10038;</span> Alcohol by volume: ${ingredient.strABV}%</li>`;
+  }
   function updateIngredients() {
     return `<div class="remove"><h2 class="modal-ingredient-title theme_text_color" id='${ingredient.idIngredient}''>${ingredient.strIngredient}</h2>
         ${type}
@@ -103,9 +110,12 @@ if (ingredient.strABV) {
   chooseAddOrRemoveButton();
 }
 
-closeBtn.addEventListener('click', ()=>{toggleModal()
-  const remove = document.querySelector('.remove')
-  remove.style.display = 'none'})
+closeBtn.addEventListener('click', () => {
+  toggleModal();
+  const remove = document.querySelector('.remove');
+  // remove.style.display = 'none';
+  document.querySelector('.remove').innerHTML = '';
+});
 function toggleModal() {
   modalIngredients.classList.toggle('is-hidden');
 }
