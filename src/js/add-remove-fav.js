4f91531ba414276baf0affa23059bf534ca287b6
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-05-19 20:30:56 +0300

    ADD: Remove btn commentary extend

--- src/js/add-remove-fav.js
+++ src/js/add-remove-fav.js
@@ -59,7 +59,7 @@ async function onBtnAddRemoveFavClick(e) {
 
     localStorage.setItem('favCocktails', JSON.stringify(favCocktails));
     Notify.info(
-      `Cocktail ${cocktailName} was removed from your favorites🙄!  If you still see item, refresh page, and it will disappear`
+      `Cocktail ${cocktailName} was removed from your favorites🙄! If you still see item, refresh page, and it will disappear`
     );
   } else if (e.target.classList.contains('btn-learn-more')) {
     onBtnAddFavClick(e);
